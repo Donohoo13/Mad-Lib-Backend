2f1b762ad50b09eb2c15e41fbf15c5c5ef13c739
Conner D <donohoo13@gmail.com>
2019-01-11 17:17:54 -0700

    express test

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ const port = 3001;
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.get('/test', (req, res) => res.send(`I don't know what I'm doing`))
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
